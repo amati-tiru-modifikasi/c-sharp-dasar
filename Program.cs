b6a36e407ad60028e87f2c6f59f91dc292861f5c
mdestafadilah <desta.08b@gmail.com>
2023-02-20 03:46:19 +0700

    equality operator

--- Program.cs
+++ Program.cs
@@ -1,17 +1,36 @@
-﻿var myBoolean = true;
-Console.WriteLine($"{nameof(myBoolean)} : {myBoolean}");
-var myChar = '\x0061';
-Console.WriteLine($"{nameof(myChar)} : {myChar}");
-var myString = "this is a string";
-Console.WriteLine($"{nameof(myString)} : {myString}");
-var myInteger = 9;
-Console.WriteLine($"{nameof(myInteger)} : {myInteger}");
-var myDecimal = 3.14m;
-Console.WriteLine($"{nameof(myDecimal)} : {myDecimal}");
+﻿// basic operator
 
-// object
+// int i = 0;
+// int j = 0;
+// Console.WriteLine(i);
+// Console.WriteLine(j);
+// j = ++i;
+// Console.WriteLine(i);
+// Console.WriteLine(j);
 
-object obj = myString;
-Console.WriteLine($"{nameof(obj)} : {obj}");
-obj = myDecimal;
-Console.WriteLine($"{nameof(obj)} : {obj}");
+// arimatic operator
+
+// int sum = 10 + 5;
+// int difference = 7 - 2;
+// int product = 5 * 3;
+// int division = 15 / 3;
+// Console.WriteLine($"{nameof(sum)}: {sum}");
+// Console.WriteLine($"{nameof(difference)}: {difference}");
+// Console.WriteLine($"{nameof(product)}: {product}");
+// Console.WriteLine($"{nameof(division)}: {division}");
+
+// int modulo = 10 % 3;
+// Console.WriteLine($"{nameof(modulo)}: {modulo}");
+
+// comparision operators
+// int int1 = 10;
+// int int2 = 20;
+
+// Console.WriteLine($"{nameof(int1)} > {nameof(int2)} ? : {int1 > int2}");
+
+
+// equality operators
+string str1 = "I love C#";
+string str2  = "I Love Javascript";
+
+Console.WriteLine($"{nameof(str1)} == {nameof(str2)} ({nameof(str1.ToLower)}) ? : {str1.ToLower() == str2.ToLower()}");
