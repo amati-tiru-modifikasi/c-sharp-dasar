e907dc8ac5afe77867fd7c32e64a80d57a9cdab8
mdestafadilah <desta.08b@gmail.com>
2023-02-20 04:12:59 +0700

    Windows Size Test

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
-﻿string myString = "I Love C#";
-Console.WriteLine($"{myString.StartsWith("\x0049")} : {myString.StartsWith("\x0049")}");
+﻿using System.Globalization;
+
+string myString = "I Love C#";
+Console.WriteLine($"{myString.StartsWith("\x0049", false, CultureInfo.CurrentCulture)} : {myString.StartsWith("\x0049")}");
 Console.WriteLine($"{myString.Length}");
+Console.SetWindowSize(250,250);
