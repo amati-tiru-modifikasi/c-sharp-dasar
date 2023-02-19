e60d6fa39299ee4a512101ff32f52c88639480df
mdestafadilah <desta.08b@gmail.com>
2023-02-19 21:31:09 +0700

    basic forward

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool myBoolean;
+myBoolean = true;
+Console.WriteLine($"{nameof(myBoolean)} : {myBoolean}");
+char myChar = '\x0061';
+Console.WriteLine($"{nameof(myChar)} : {myChar}");
+string myString = "this is a string";
+Console.WriteLine($"{nameof(myString)} : {myString}");
+int myInteger = 9;
+Console.WriteLine($"{nameof(myInteger)} : {myInteger}");
+decimal myDecimal = 3.14m;
+Console.WriteLine($"{nameof(myDecimal)} : {myDecimal}");
+
+// object
+
+object obj = myString;
+Console.WriteLine($"{nameof(obj)} : {obj}");
+obj = myDecimal;
+Console.WriteLine($"{nameof(obj)} : {obj}");
