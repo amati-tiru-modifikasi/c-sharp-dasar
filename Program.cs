86fed8fad583f28868093894d80011370182c108
mdestafadilah <desta.08b@gmail.com>
2023-03-09 04:38:52 +0700

    try catch example

--- Program.cs
+++ Program.cs
@@ -2,8 +2,19 @@
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Enter your number: ");
-        int num = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine(Math.Sqrt(num));
+        try
+        {
+            Console.WriteLine("Enter your number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Math.Sqrt(num));
+            Console.ReadKey(); // press any key
+        }
+        catch (System.Exception ex)
+        {
+            Console.WriteLine($"There is something wrong in our application, please look at this message: {ex.Message}");
+            Console.ReadKey(); // press any key
+
+        }
+
     }
 }
