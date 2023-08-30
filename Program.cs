ec873676b9bb36f5f1295a0759e93dad77dacb01
mDestaFadilah <desta.08b@gmail.com>
2023-08-30 15:18:08 +0700

    math max check value

--- Program.cs
+++ Program.cs
@@ -4,15 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {   
-        Random dice = new Random();
-
-        int roll1 = dice.Next(); // 0 to 2,147,483,647
-        int roll2 = dice.Next(101); // 0 to 100
-        int roll3 = dice.Next(50, 101); // 50 to 100
-
-        Console.WriteLine($"First roll: {roll1}");
-        Console.WriteLine($"Second roll: {roll2}");
-        Console.WriteLine($"Third roll: {roll3}");
-        
+        int firstValue = 500;
+        int secondValue = 600;
+        int largerValue;
+        largerValue = Math.Max(firstValue, secondValue);
+        Console.WriteLine(largerValue);
+                        
     }
 }
