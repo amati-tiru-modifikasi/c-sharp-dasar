9f2ae6f84539a272ae1a8904c1b26b062b644802
mDestaFadilah <desta.08b@gmail.com>
2023-08-30 15:14:07 +0700

    Overloaded methods

--- Program.cs
+++ Program.cs
@@ -3,9 +3,16 @@
 class Program
 {
     static void Main(string[] args)
-    {
+    {   
         Random dice = new Random();
-        Console.WriteLine(dice.Next(1, 7));
 
+        int roll1 = dice.Next(); // 0 to 2,147,483,647
+        int roll2 = dice.Next(101); // 0 to 100
+        int roll3 = dice.Next(50, 101); // 50 to 100
+
+        Console.WriteLine($"First roll: {roll1}");
+        Console.WriteLine($"Second roll: {roll2}");
+        Console.WriteLine($"Third roll: {roll3}");
+        
     }
 }
