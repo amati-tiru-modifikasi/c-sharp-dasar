19a285ff329622f7e5b9153eb9087eb5f0099951
mDestaFadilah <desta.08b@gmail.com>
2023-08-31 10:05:58 +0700

    do-while-chalange

--- Program.cs
+++ Program.cs
@@ -4,29 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {   
-        Random random = new Random();
-        int current = random.Next(1, 11);
+        int hero = 10;  
+        int monster = 10;
 
-        /*
-        do
-        {
-            current = random.Next(1, 11);
-            Console.WriteLine(current);
-        } while (current != 7);
-        */
+        Random dice = new Random();
 
-        do {
-            current = random.Next(1, 11);
-            if(current >= 8) continue;
-            Console.WriteLine(current);
-        }while (current != 7);
-        
-        /*
-        while (current >= 3) {
-            Console.WriteLine("Nilainya: " + current);
-            current = random.Next(1,11);
-        }
-        Console.WriteLine($"Last number: {current}");
-        */
+        do{
+            int roll = dice.Next(1,11);
+            monster -= roll;
+            Console.WriteLine($"Monster was damaged and lost {roll} health and now has {monster} health.");
+
+            if (monster <= 0) continue;
+
+            roll = dice.Next(1,11);
+            hero -= roll;
+            Console.WriteLine($"Hero was damaged and lost {roll} health and now has {hero} health.");
+
+        }while( hero > 0 && monster > 0);
+
+        Console.WriteLine(hero > monster ? "Hero wins!" : "Monster wins!");
+
     }
 }
