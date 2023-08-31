b6044a0595f891d60b1917dd5ca9fd5e3ea608e3
mDestaFadilah <desta.08b@gmail.com>
2023-08-31 08:19:03 +0700

    variable scope

--- Program.cs
+++ Program.cs
@@ -4,8 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {   
-        Random koin = new Random();
-        int flip = koin.Next(0, 2);
-        Console.WriteLine((flip == 0) ? "Heads": "Tails");
+        int[] nomors = { 4,8,15,16,23,42 };
+        int total = 0;
+        bool found = false;
+
+        foreach (int nomor in nomors)
+        {
+            total += nomor;
+
+            if (nomor == 42)
+            {
+                found = true;
+            }
+        }
+
+        if(found){
+            Console.WriteLine($"{found} Set contains 42");
+        }
+
+        Console.WriteLine($"Total: {total}");
     }
 }
