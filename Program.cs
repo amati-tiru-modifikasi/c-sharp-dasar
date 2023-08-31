7d9e942d37488091857b254fe3dd77a8735822fe
mDestaFadilah <desta.08b@gmail.com>
2023-08-31 09:59:08 +0700

    random

--- Program.cs
+++ Program.cs
@@ -5,12 +5,19 @@ class Program
     static void Main(string[] args)
     {   
         Random random = new Random();
-        int current = 0;
+        int current = random.Next(1, 11);
 
+        /*
         do
         {
             current = random.Next(1, 11);
             Console.WriteLine(current);
         } while (current != 7);
+        */
+        while (current >= 3) {
+            Console.WriteLine("Nilainya: " + current);
+            current = random.Next(1,11);
+        }
+        Console.WriteLine($"Last number: {current}");
     }
 }
