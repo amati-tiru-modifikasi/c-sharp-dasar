9ab76023c0d737d93f09d8956e2a55c3367dcf6e
mDestaFadilah <desta.08b@gmail.com>
2023-08-31 07:49:29 +0700

    compare 2 values

--- Program.cs
+++ Program.cs
@@ -10,6 +10,10 @@ class Program
 
         string myValue = "a";
         Console.WriteLine(myValue == "a");
+
+        string value1 = " a";
+        string value2 = "A ";
+        Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
                         
     }
 }
