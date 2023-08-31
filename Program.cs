0c105728576722d39ee621ba89cb00cbf44ad18d
mDestaFadilah <desta.08b@gmail.com>
2023-08-31 07:52:37 +0700

    random number

--- Program.cs
+++ Program.cs
@@ -4,16 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {   
-        Console.WriteLine("a" == "a");
-        Console.WriteLine("a" == "A");
-        Console.WriteLine(1 == 2);
-
-        string myValue = "a";
-        Console.WriteLine(myValue == "a");
-
-        string value1 = " a";
-        string value2 = "A ";
-        Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
-                        
+        Random koin = new Random();
+        int flip = koin.Next(0, 2);
+        Console.WriteLine((flip == 0) ? "Heads": "Tails");
     }
 }
