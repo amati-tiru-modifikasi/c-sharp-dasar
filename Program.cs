dbd3ce015de5cea86e5271d038dc83c31515b3b3
mDestaFadilah <desta.08b@gmail.com>
2023-08-31 07:47:24 +0700

    basic boolean

--- Program.cs
+++ Program.cs
@@ -4,11 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {   
-        int firstValue = 500;
-        int secondValue = 600;
-        int largerValue;
-        largerValue = Math.Max(firstValue, secondValue);
-        Console.WriteLine(largerValue);
+        Console.WriteLine("a" == "a");
+        Console.WriteLine("a" == "A");
+        Console.WriteLine(1 == 2);
+
+        string myValue = "a";
+        Console.WriteLine(myValue == "a");
                         
     }
 }
