1c03c9a51aba58dcf77eb1f31629d505984773f0
mDestaFadilah <desta.08b@gmail.com>
2023-08-31 20:59:01 +0700

    Do-While string Part 2

--- Program.cs
+++ Program.cs
@@ -5,35 +5,27 @@ class Program
     static void Main(string[] args)
     {   
         string? readResult;
-        string valueEntered = "";
-        int numValue = 0;
-        bool validNumber = false;
-
-        Console.WriteLine("Enter an integer value between 5 and 10");
+        string roleName = "";
+        bool validEntry = false;
 
         do{
+            Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
             readResult = Console.ReadLine();
+
             if(readResult != null)
             {
-                valueEntered = readResult;
+                roleName = readResult.Trim();
             }
 
-            validNumber = int.TryParse(valueEntered, out numValue);
-
-            if (validNumber == true)
+            if(roleName.ToLower() == "administrator" || roleName.ToLower() == "manager" || roleName.ToLower() == "user")
             {
-                if (numValue <= 5 || numValue >= 10)
-                {
-                    validNumber = false;
-                    Console.WriteLine($"Your entered {numValue}. Please enter a number between 5 and 10");
-                }
+                validEntry = true;
             }else{
-                Console.WriteLine("Sorry, you entered an invalid number, please try again");
+                Console.WriteLine($"The role name that you entered, \"{roleName}\" is not valid");
             }
-        }while(validNumber == false);
+        }while(validEntry == false);
 
-        Console.WriteLine($"Your input value ({numValue}) has been accepted");
-
+        Console.WriteLine($"Your input value ({roleName}) has been accepted");
         readResult = Console.ReadLine();
     }
 }
