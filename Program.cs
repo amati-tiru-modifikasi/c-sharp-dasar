fe9e332908519f0a6e7be70934165ac22e4f072e
mDestaFadilah <desta.08b@gmail.com>
2023-08-31 21:04:16 +0700

    do while extract if include dot (.)

--- Program.cs
+++ Program.cs
@@ -4,28 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {   
-        string? readResult;
-        string roleName = "";
-        bool validEntry = false;
+        string[] myStrings = new string[2] { "I Like Drink Coffe. I Drink with Ice", "I Like Drink Tea" };
+        int stringCount = myStrings.Length;
 
-        do{
-            Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
-            readResult = Console.ReadLine();
+        string myString = "";
+        int periodLocation = 0;
 
-            if(readResult != null)
+        for (int i = 0; i < stringCount; i++)
+        {
+            myString = myStrings[i];
+            periodLocation = myString.IndexOf(".");
+
+            string mySentence;
+
+            while (periodLocation != -1)
             {
-                roleName = readResult.Trim();
+                // first sentence is the string value to the left of the period location
+                mySentence = myString.Remove(periodLocation);
+
+                // the remainder of myString is the string value to the right of the location
+                myString = myString.Substring(periodLocation + 1);
+
+                // remove any leading white-space from myString
+                myString = myString.TrimStart();
+
+                // update the comma location and increment the counter
+                periodLocation = myString.IndexOf(".");
+
+                Console.WriteLine(mySentence);
             }
 
-            if(roleName.ToLower() == "administrator" || roleName.ToLower() == "manager" || roleName.ToLower() == "user")
-            {
-                validEntry = true;
-            }else{
-                Console.WriteLine($"The role name that you entered, \"{roleName}\" is not valid");
-            }
-        }while(validEntry == false);
+             mySentence = myString.Trim();
+            Console.WriteLine(mySentence);
 
-        Console.WriteLine($"Your input value ({roleName}) has been accepted");
-        readResult = Console.ReadLine();
+        }
     }
 }
