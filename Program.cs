e4e3f1386a3d15dd401c1527ea3b0ace613165d3
mDestaFadilah <desta.08b@gmail.com>
2023-09-03 20:56:13 +0700

    string array add

--- Program.cs
+++ Program.cs
@@ -4,15 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {   
-        string value = "bad";
-        int result = 0;
-        if (int.TryParse(value, out result)) {
-            Console.WriteLine($"result: {result}");
-        }else{
-            Console.WriteLine("Gagal dikonversi");
+        string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+
+        decimal total = 0m;
+        string message = "";
+
+        foreach (string item in values)
+        {
+            decimal number;
+            if(decimal.TryParse(item, out number)){
+                total += number;
+            }else{
+                message += item;
+            }
         }
 
-        if (result > 0)
-            Console.WriteLine($"Terukur dengan offset: {50 + result}");
+        Console.WriteLine($"Message: {message}");
+        Console.WriteLine($"Total: {total}");
+
+        // string value = "bad";
+        // int result = 0;
+        // if (int.TryParse(value, out result)) {
+        //     Console.WriteLine($"result: {result}");
+        // }else{
+        //     Console.WriteLine("Gagal dikonversi");
+        // }
+
+        // if (result > 0)
+        //     Console.WriteLine($"Terukur dengan offset: {50 + result}");
     }
 }
