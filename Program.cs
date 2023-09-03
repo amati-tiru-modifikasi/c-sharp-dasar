64c9b5c4ec3f99aa0e309b5134b5fb92d9964c8d
mDestaFadilah <desta.08b@gmail.com>
2023-09-03 21:05:02 +0700

    chalange value type done

--- Program.cs
+++ Program.cs
@@ -4,23 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {   
-        string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+        int value1 = 12;
+        decimal value2 = 6.2m;
+        float value3 = 4.3f;
 
-        decimal total = 0m;
-        string message = "";
+        int result1 = Convert.ToInt32((decimal)value1 / value2);
+        decimal result2 = value2 / (decimal)value3;
+        float result3 = value3 / value1;
+     
+        // Your code here to set result1
+        // Hint: You need to round the result to nearest integer (don't just truncate)
+        Console.WriteLine($"Pembagian value1 by value2, display the result as an int: {result1}");
 
-        foreach (string item in values)
-        {
-            decimal number;
-            if(decimal.TryParse(item, out number)){
-                total += number;
-            }else{
-                message += item;
-            }
-        }
+        // Your code here to set result2
+        Console.WriteLine($"Pembagian value2 by value3, display the result as a decimal: {result2}");
 
-        Console.WriteLine($"Message: {message}");
-        Console.WriteLine($"Total: {total}");
+        // Your code here to set result3
+        Console.WriteLine($"Pembagian value3 by value1, display the result as a float: {result3}");
 
         // string value = "bad";
         // int result = 0;
