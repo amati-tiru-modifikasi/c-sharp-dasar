6f1c07a8347b9d7f2a8affde0b82763510bfc212
mDestaFadilah <desta.08b@gmail.com>
2023-09-03 20:45:55 +0700

    using tryParse() an string to integer

--- Program.cs
+++ Program.cs
@@ -4,7 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {   
-        string name = "desta";
-        Console.WriteLine(int.Parse(name));
+        string value = "102";
+        int result = 0;
+        if (int.TryParse(value, out result)) {
+            Console.WriteLine($"result: {result}");
+        }else{
+            Console.WriteLine("Gagal dikonversi");
+        }
     }
 }
