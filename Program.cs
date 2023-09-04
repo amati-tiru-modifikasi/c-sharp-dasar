7c446c06ace8fd4f5f08af4152169b59efa8f85a
mDestaFadilah <desta.08b@gmail.com>
2023-09-04 14:06:51 +0700

    array of string pallets

--- Program.cs
+++ Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {   
-       // TODO: Perform operations on arrays using helper methods in C#
+        string[] pallets = { "B14", "A11", "B12", "A13" };        
+        
+        Console.WriteLine($"Sorted {pallets}");
+        Array.Sort(pallets);
+        foreach (var pallet in pallets)
+        {
+            Console.WriteLine($"-- {pallet}");
+        }
     }
 }
