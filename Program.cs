d2ef3c932383b275dba162fde11a3a6b4e2c56fc
mDestaFadilah <desta.08b@gmail.com>
2023-09-04 14:19:45 +0700

    error

--- Program.cs
+++ Program.cs
@@ -44,5 +44,17 @@ class Program
         {
             Console.WriteLine($"-- {pallet}");
         }
+
+        // Check Null
+        Console.WriteLine("");
+        Console.WriteLine("Check null...");
+        Console.WriteLIne($"Before: {pallets[0].ToLower()}");
+        Array.Clear(pallets,0,2);
+        Console.WriteLine($"After: {pallets[0].ToLower()}");
+        Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+        foreach (var pallet in pallets)
+        {
+            Console.WriteLine($"-- {pallet}");
+        }
     }
 }
