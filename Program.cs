2161b4d5e444ca44e5ddbc14cc8aa0e10bf3d929
mDestaFadilah <desta.08b@gmail.com>
2023-09-05 23:35:06 +0700

    PAUSE: https://learn.microsoft.com/en-us/training/modules/csharp-format-strings/

--- Program.cs
+++ Program.cs
@@ -4,21 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {   
-        string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
-        string[] items = orderStream.Split(", ");
-        Array.Sort(items);
-
-        foreach (string item in items)
-        {
-            if (item.Length == 4)
-            {
-                Console.WriteLine(item);
-            }
-            else
-            {
-                Console.WriteLine(item + "\t- Error");
-            }
-        }
+        // Next: Format Alphanumeric Data for Presentation in C#
 
     }
 }
