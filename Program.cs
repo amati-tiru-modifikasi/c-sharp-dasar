686cabb0ae9bd948ba24226d382ed656c0f76620
mDestaFadilah <desta.08b@gmail.com>
2023-09-05 22:15:57 +0700

    comma-separated-value string using Join()

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@ class Program
         string value = "abc123";
         char[] valueArray = value.ToCharArray();
         Array.Reverse(valueArray);
-        string result  = new string(valueArray);
+        // string result  = new string(valueArray);
+        string result = String.Join(",",valueArray);
         Console.WriteLine(result);
     }
 }
