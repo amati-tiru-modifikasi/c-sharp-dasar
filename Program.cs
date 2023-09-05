bd6436b669715ae38a69af65399cc3df6106fb35
mDestaFadilah <desta.08b@gmail.com>
2023-09-05 23:34:15 +0700

    chalange 2 done

--- Program.cs
+++ Program.cs
@@ -4,23 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {   
-        string value = "The quick brown fox jumps over the lazy dog";
+        string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+        string[] items = orderStream.Split(", ");
+        Array.Sort(items);
 
-        // Step 1
-        string[] message = value.Split(' ');
-
-        // Step 2
-        string[] newMessage = new string[message.Length];
-
-        // Step 3
-        for (int i = 0; i < message.Length; i++) {
-            char[] letters = message[i].ToCharArray(); 
-            Array.Reverse(letters);
-            newMessage[i] = new string(letters);
+        foreach (string item in items)
+        {
+            if (item.Length == 4)
+            {
+                Console.WriteLine(item);
+            }
+            else
+            {
+                Console.WriteLine(item + "\t- Error");
+            }
         }
 
-        // Step 4
-        string result = String.Join(" ", newMessage);
-        Console.WriteLine(result);
     }
 }
