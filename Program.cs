add92af01c21565fdbda418aaf8104b002969ae9
mDestaFadilah <desta.08b@gmail.com>
2023-09-06 07:53:14 +0700

    format with region

--- Program.cs
+++ Program.cs
@@ -4,10 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {   
-        string first = "hello";
-        string second = "world";
-        Console.WriteLine($"{first} {second}!");
-        Console.WriteLine($"{second} {first}!");
-        Console.WriteLine($"{first} {first} {first}!");
+        decimal price = 123.45m;
+        int discount = 50;
+        Console.WriteLine($"Price: {price:C} (Save {discount:C})");
     }
 }
