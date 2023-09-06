b5967c33ad04704135c7817a57454eae4d874c3c
mDestaFadilah <desta.08b@gmail.com>
2023-09-06 21:08:45 +0700

    output the writeline with PadRight()

--- Program.cs
+++ Program.cs
@@ -9,8 +9,25 @@ class Program
         // string result = string.Format("{0} {1}!", first, second);
         // Console.WriteLine(result);
 
-        string input = "Pad this";
-        Console.WriteLine(input.PadRight(12, '-'));	
+        // string input = "Pad this";
+        // Console.WriteLine(input.PadRight(12, '-'));	
+
+        // string paymentId = "769C";
+        // var formattedLine = paymentId.PadRight(6);
+        // Console.WriteLine(formattedLine);
+
+        string paymentId = "769";
+        string payeeName = "Mr. Stephen Ortega";
+        string paymentAmount = "$5,000.00";
+
+        var formattedLine = paymentId.PadRight(6);
+        formattedLine += payeeName.PadRight(24);
+        formattedLine += paymentAmount.PadRight(10);
+
+        // sama dengan ini
+        // formattedLine = formattedLine + payeeName.PadRight(24);
+
+        Console.WriteLine(formattedLine);
     
     }
 }
