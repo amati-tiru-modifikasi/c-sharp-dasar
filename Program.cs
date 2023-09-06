58098dfc0b826bc483aa6bb96767392dc71fb77f
mDestaFadilah <desta.08b@gmail.com>
2023-09-06 20:45:50 +0700

    DISCOVER MORE STRING INTERPOLATION

--- Program.cs
+++ Program.cs
@@ -4,12 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {   
-        decimal price = 67.55m;
-        decimal salePrice = 59.99m;
+        int invoiceNumber = 1201;
+        decimal productShares = 25.4568m;
+        decimal subtotal = 2750.00m;
+        decimal taxPercentage = .15825m;
+        decimal total = 3185.19m;
 
-        string yourDiscount = String.Format("You saved {0:C2} of the regular {1:C2} price",(price - salePrice), price);
-        yourDiscount += $" A discount of {((price - salePrice)/price):P2}!"; //inserted
-        
-        Console.WriteLine(yourDiscount);
+        Console.WriteLine($"Invoice Number: {invoiceNumber}");
+        Console.WriteLine($"Shares: {productShares:N3} Product");
+        Console.WriteLine($"Sub Total: {subtotal:C} Product");
+        Console.WriteLine($"Tax: {taxPercentage:P2} Product");
+        Console.WriteLine($"Total Billed: {total:C} Product");
+    
     }
 }
