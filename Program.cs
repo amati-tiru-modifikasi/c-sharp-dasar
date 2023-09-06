3422445945b37a5e9cf8da066769ae9cc9620cbe
mDestaFadilah <desta.08b@gmail.com>
2023-09-06 08:38:32 +0700

    using interpolation

--- Program.cs
+++ Program.cs
@@ -4,7 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {   
-        decimal tax = .36785m;
-        Console.WriteLine($"Tax rate: {tax:P2}");
+        decimal price = 67.55m;
+        decimal salePrice = 59.99m;
+
+        string yourDiscount = String.Format("You saved {0:C2} of the regular {1:C2} price",(price - salePrice), price);
+        Console.WriteLine(yourDiscount);
     }
 }
