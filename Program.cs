8fdef38b9cb44541037a914003781f10665d33fc
mDestaFadilah <desta.08b@gmail.com>
2023-09-07 13:32:21 +0700

    Chap 2 - IndexOf() gives you the first position of a character or string inside of another string.
    - IndexOf() returns -1 if it can't find a match.
    
    - Substring() returns just the specified portion of a string, using a starting position and optional length.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {   
-        // Modify the Content of Strings Using Built-In String Data Type Methods in C#
-    
+        string message = "Find what is (inside the parentheses)";
+        string messageDua = "What is the value <span>between the tags</span>?";
+
+        int openingPosition = message.IndexOf('(');
+        int closingPosition = message.IndexOf(')');
+
+        const string openSpan = "<span>";
+        const string closingSpan = "</span>";
+        int openingPositionDua = messageDua.IndexOf(openSpan);
+        int closingPositionDua = messageDua.IndexOf(closingSpan);
+
+        Console.WriteLine("");
+        Console.WriteLine("Lenght panjang...");
+        Console.WriteLine(openingPosition);
+        Console.WriteLine(closingPosition);
+
+        Console.WriteLine("");
+        Console.WriteLine("Between parenthis...");
+
+        int panjang = closingPosition - openingPosition;
+        Console.WriteLine(message.Substring(openingPosition, panjang)); 
+
+        Console.WriteLine("");
+        Console.WriteLine("Modify posistion of string");
+
+        openingPosition += 1;
+        int panjangDua = closingPosition - openingPosition;
+        Console.WriteLine(message.Substring(openingPosition, panjangDua));
+
+
+        Console.WriteLine("");
+        Console.WriteLine("Message ke dua");
+
+        openingPositionDua += openSpan.Length;
+        int length = closingPositionDua - openingPositionDua;
+        Console.WriteLine(messageDua.Substring(openingPositionDua, length));
     }
 }
