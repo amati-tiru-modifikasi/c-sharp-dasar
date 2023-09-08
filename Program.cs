545a79ba7b64b5a80a23fd91d4f2d285f013077c
mDestaFadilah <desta.08b@gmail.com>
2023-09-08 21:16:31 +0700

    PAUSE: https://learn.microsoft.com/en-us/training/modules/guided-project-work-variable-data-c-sharp/

--- Program.cs
+++ Program.cs
@@ -4,39 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {   
-        const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+        // Guided Project - Work with Variable Data in C#
 
-        string quantity = "";
-        string output = "";
-
-        // Your work here
-        Console.WriteLine($"Kalimat soal: {input}");
-
-        // extract quantity
-        int quantityStart = input.IndexOf("<span>");
-        int quantityEnd = input.IndexOf("</span>");
-
-        quantityStart += "<span>".Length; // added lenght of tag
-        int quantityLength = quantityEnd - quantityStart;
-        quantity = input.Substring(quantityStart, quantityLength);
-        quantity = $"Quantity: {quantity}";
-
-        // set output to input, replacing the trademark simbol with registred tradmark
-        output = input.Replace("&trade;","&reg;");
-
-        // remove div opening
-        int divStart = input.IndexOf("<div>");
-        int divLength = "<div>".Length;
-        output = output.Remove(divStart,divLength);
-
-        // remove div closing
-        int divEnd = output.IndexOf("</div>");
-        int divEndLength = "</div>".Length;
-        output = output.Remove(divEnd, divEndLength);
-
-        output = $"Output: {output}";
-
-        Console.WriteLine(quantity);
-        Console.WriteLine(output);
     }
 }
