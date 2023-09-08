88866196794d82caa4e70ce87427048e611fd898
mDestaFadilah <desta.08b@gmail.com>
2023-09-08 20:42:29 +0700

    Use the Remove() method

--- Program.cs
+++ Program.cs
@@ -4,7 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {   
-        // Exercise - Use the Remove() and Replace() methods
+        // Remove characters in specific locations from a string
+        string data = "12345John Smith          5000  3  ";
+        string updateData = data.Remove(5, 20);
+        Console.WriteLine(updateData);
 
+        
     }
 }
