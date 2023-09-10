ecd0f88f1d90705b245697d86ea9c7896a4b6fb4
mDestaFadilah <desta.08b@gmail.com>
2023-09-11 04:13:28 +0700

    update an looping array cat

--- Program.cs
+++ Program.cs
@@ -103,7 +103,7 @@ class Program
                         if (ourAnimals[i, 0] != "ID #: ")
                         {
                             Console.WriteLine();
-                            for (int j = 0; j < 6; j++)
+                            for (int j = 0; j < 7; j++)
                             {
                                 Console.WriteLine(ourAnimals[i, j]);
                             }
