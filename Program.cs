96d491118e45bf11a5a429b2c722bbab1b6637e1
mDestaFadilah <desta.08b@gmail.com>
2023-09-12 04:05:20 +0700

    WHEN IN DOUBT int for most whole numbers decimal for numbers representing money bool for true or false values string for alphanumeric value

--- Program.cs
+++ Program.cs
@@ -4,14 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {   
-        // int[] data;
-        // data = new int[3];
-
-        // single line code same thing
-        int[] data = new int[3];
-
-        string shortenedString = "Hello World!";
-        Console.WriteLine(shortenedString);
+        // RECAP
 
     }
 }
