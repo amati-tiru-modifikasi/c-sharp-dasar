ca18aaadca3272c7fffde22c0041d62fb0e19c9c
mDestaFadilah <desta.08b@gmail.com>
2023-09-14 03:51:31 +0700

    RECAP: Information can be passed to methods in the form of parameters. Parameters are declared in the method signature. Multiple parameters are separated by commas. Methods can accept variable or literal arguments.

--- Program.cs
+++ Program.cs
@@ -4,16 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        ```c#
-        CountTo(5);
+        int[] jadwal = { 800, 1200, 1600, 2000 };
+        DisplayPengaturanWaktu(jadwal, 6, -6);
 
-        void CountTo(int max) 
+        void DisplayPengaturanWaktu(int[] times, int currGMT, int newGMT)
         {
-            for (int i = 0; i < max; i++)
+            int diff = 0; // give range value
+            if (Math.Abs(newGMT) > 12 || Math.Abs(currGMT) > 12) // absolute value
             {
-                Console.Write($"${i}, ");
+                Console.WriteLine("Invalid GMT");
+            }
+            else if (newGMT <= 0 && currGMT <= 0 || newGMT >= 0 && currGMT >= 0)
+            {
+                diff = 100 * (Math.Abs(newGMT) - Math.Abs(currGMT));
+            }
+            else
+            {
+                diff = 100 * (Math.Abs(newGMT) + Math.Abs(currGMT));
+            }
+
+            for (int i = 0; i < times.Length; i++)
+            {
+                int newTime = ((times[i] + diff)) % 2400;
+                Console.WriteLine($"{times[i]} -> {newTime}");
             }
         }
-        ```
     }
 }
