c8ce3024f7ccc9c6909955bebc1b9bfb17c3a6a8
mDestaFadilah <desta.08b@gmail.com>
2023-09-14 03:52:25 +0700

    PAUSE: https://learn.microsoft.com/en-us/training/modules/create-c-sharp-methods-parameters/3-exercise-understand-method-scope

--- Program.cs
+++ Program.cs
@@ -4,30 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        int[] jadwal = { 800, 1200, 1600, 2000 };
-        DisplayPengaturanWaktu(jadwal, 6, -6);
+        // PAUSE: Exercise - Understand method scope
 
-        void DisplayPengaturanWaktu(int[] times, int currGMT, int newGMT)
-        {
-            int diff = 0; // give range value
-            if (Math.Abs(newGMT) > 12 || Math.Abs(currGMT) > 12) // absolute value
-            {
-                Console.WriteLine("Invalid GMT");
-            }
-            else if (newGMT <= 0 && currGMT <= 0 || newGMT >= 0 && currGMT >= 0)
-            {
-                diff = 100 * (Math.Abs(newGMT) - Math.Abs(currGMT));
-            }
-            else
-            {
-                diff = 100 * (Math.Abs(newGMT) + Math.Abs(currGMT));
-            }
-
-            for (int i = 0; i < times.Length; i++)
-            {
-                int newTime = ((times[i] + diff)) % 2400;
-                Console.WriteLine($"{times[i]} -> {newTime}");
-            }
-        }
     }
 }
