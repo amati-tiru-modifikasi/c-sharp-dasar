006b4496f4bffaad1eabb2c9095a36d9d7957707
mDestaFadilah <desta.08b@gmail.com>
2023-09-13 13:34:41 +0700

    masih error

--- Program.cs
+++ Program.cs
@@ -1,3 +1,59 @@
 ﻿using System;
 
-// TODO: https://learn.microsoft.com/en-us/training/modules/write-first-c-sharp-method/5-exercise-build-code-with-methods
+class Program
+{
+    static void Main(string[] args)
+    {   
+        string[] ipv4Input = {"107.31.1.5", "255.0.0.255", "555..0.555", "255...255"};
+        string[] address;
+        bool validLength = false;
+        bool validZeroes = false;
+        bool validRanges = false;
+
+        foreach (string ip in ipv4Input)
+        {
+            ValidateLength();
+            ValidateZeroes();
+            ValidateRanges();
+
+            if (validLength && validZeroes && validRanges)
+            {
+                Console.WriteLine($"{ip} Is valid IPv4 address");
+            }else{
+                Console.WriteLine($"{ip} is an invalid IPv4 address");
+            }
+        }
+
+        void ValidateLength() {
+            string[] address = ipv4Input.Split(".");
+            validLength = address.Length == 4;
+        }
+
+        void ValidateZeroes(){
+            string[] address = ipv4Input.Split(".");
+            foreach (string number in address)
+            {
+                if (number.Length > 1 && number.StartWith("0"))
+                {
+                    validZeroes = false;
+                    return;
+                }
+            }
+            validZeroes = true;
+        }
+
+        void ValidateRanges(){
+            string[] address = ipv4Input.Split(".");
+            foreach (string number in address)
+            {
+                if (int.Parse(number) > 255)
+                {
+                    validRanges = false;
+                    return;
+                }
+            }
+            validRanges = true;
+        }
+
+    }
+}
