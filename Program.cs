74f529b1a9ca6e2a40b7e545c6de188b67dcabe0
mDestaFadilah <desta.08b@gmail.com>
2023-09-15 04:13:08 +0700

    PAUSE: https://learn.microsoft.com/en-us/training/modules/create-c-sharp-methods-parameters/5-exercise-use-named-optional-parameters

--- Program.cs
+++ Program.cs
@@ -4,28 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        double pi = 3.14159;
-
-        PrintCicleInfo(12);
-        PrintCicleInfo(33);
-
-        void PrintCicleArea(int radius) 
-        {
-            double area = pi * (radius * radius);
-            Console.WriteLine($"Area = {area}");
-        }
-
-        void PrintCirclCircumference(int radius)
-        {
-            double circumeference = 2 * pi * radius;
-            Console.WriteLine($"Circumference = {circumeference}");
-        }
-
-        void PrintCicleInfo(int radius)
-        {
-            Console.WriteLine($"Circle with radius {radius}");
-            PrintCicleArea(radius);
-            PrintCirclCircumference(radius);
-        }
+        // TODO: Exercise - Methods with optional parameters
     }
 }
