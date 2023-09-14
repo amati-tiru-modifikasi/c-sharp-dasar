b4f5f89a6ce853d7967308f5950a49200d44ac91
mDestaFadilah <desta.08b@gmail.com>
2023-09-15 03:00:21 +0700

    TEST VARIABLE SCOPE

--- Program.cs
+++ Program.cs
@@ -4,7 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        // PAUSE: Exercise - Understand method scope
+        // Global student array
+        string[] students = {"Jenna","Alesha","Haikal","Ziyad","Arshaka"};
 
+        DisplayStudents(students);
+        DisplayStudents(new string[] {"Putri","Zakiyah"}); // added more students
+
+        // function to view student
+        void DisplayStudents(string[] students)
+        {
+            foreach (string student in students)
+            {
+                Console.WriteLine($"{student}, ");
+            }
+            Console.WriteLine();
+        }
+
+        
+        Console.WriteLine("");
+        Console.WriteLine();
     }
 }
