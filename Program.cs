b4da348571013c2e5bbd22461bec2f7e03153545
mDestaFadilah <desta.08b@gmail.com>
2023-09-26 13:24:47 +0700

    Review the solution to display email addresses

--- Program.cs
+++ Program.cs
@@ -4,50 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        string[] guestList = {"Rebecca", "Nadia", "Noor", "Jonte"};
-        string[] rsvps = new string[10];
-        int count = 0;
-
-        RSVP("Rebecca");
-        RSVP("Nadia", 2, "Nuts");
-        RSVP(name: "Linh", partySize: 2, inviteOnly: false);
-        RSVP("Tony", allergies: "Jackfruit", inviteOnly: true);
-        RSVP("Noor", 4, inviteOnly: false);
-        RSVP("Jonte", 2, "Stone fruit", false);
-        ShowRSVPs();
-
-        void RSVP(string name, int partySize = 1, string allergies = "none", bool inviteOnly = true)
+        string[,] corporate = 
         {
-            if(inviteOnly)
-            {
-                bool found = false;
-                foreach (string guest in guestList)
-                {
-                    if (guest.Equals(name))
-                    {
-                        found = true;
-                        break;
-                    }
+            {"Robert", "Bavin"}, {"Simon", "Bright"},
+            {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+            {"Sarah", "Delucchi"}, {"Sinan", "Ali"}};
 
-                    if (!found)
-                    {
-                        Console.WriteLine($"Sorry, {name} is not on the guest list");
-                        return;
-                    }
-                }
-            }
+        string[,] external = 
+        {
+            {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+            {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+        };
 
-            rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
-            count++;
+        string externalDomain = "hayworth.com";
+
+        for (int i = 0; i < corporate.GetLength(0); i++) 
+        {
+            DisplayEmail(first: corporate[i,0], last: corporate[i,1]);
         }
 
-        void ShowRSVPs()
+        for (int i = 0; i < external.GetLength(0); i++) 
         {
-            Console.WriteLine("\nTotal RSVPS: ");
-            for (int i = 0; i < count; i++)
-            {
-                Console.WriteLine(rsvps[i]);
-            }
+            DisplayEmail(first: external[i,0], last: external[i,1], domain: externalDomain);
+        }
+
+        void DisplayEmail(string first, string last, string domain = "contoso.com") 
+        {
+            string email = first.Substring(0, 2) + last;
+            email = email.ToLower();
+            Console.WriteLine($"{email}@{domain}");
         }
     }
 }
