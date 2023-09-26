0d96f9b1da84462ade02d6219b58b2b0a9c86962
mDestaFadilah <desta.08b@gmail.com>
2023-09-26 13:45:05 +0700

    PAUSE: https://learn.microsoft.com/en-us/training/modules/create-c-sharp-methods-return-values/

--- Program.cs
+++ Program.cs
@@ -5,6 +5,6 @@ class Program
     static void Main(string[] args)
     {
         // TODO: Create C# methods that return values
-
+        // https://learn.microsoft.com/en-us/training/modules/create-c-sharp-methods-return-values/
     }
 }
