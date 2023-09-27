e147828a2a01daf9701b08ce058caa87d973fcaa
mDestaFadilah <desta.08b@gmail.com>
2023-09-27 21:44:53 +0700

    Create a method that returns a double

--- Program.cs
+++ Program.cs
@@ -5,14 +5,21 @@ class Program
     static void Main(string[] args)
     {
         double usd = 23.73;
-        int idr = UsedToIdr(usd);
+        int idr = UsdToIdr(usd);
 
         Console.WriteLine($"${usd} USD = Rp.{idr} rupiah");
+        Console.WriteLine($"Rp.{idr} IDR = ${IdrToUsd(idr)} USD");
 
-        int UsedToIdr(double usd)
+        int UsdToIdr(double usd)
         {
-            int rate = 15000;
+            int rate = 15500;
             return (int) (rate * usd);
+        }
+
+        double IdrToUsd(int idr)
+        {
+            int rate = 15500;
+            return idr / rate;
         }
 
     }
