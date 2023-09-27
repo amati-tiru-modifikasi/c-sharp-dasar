ecdf80e7fb6283d59f9cfac38e619eef068c56e4
mDestaFadilah <desta.08b@gmail.com>
2023-09-27 21:29:33 +0700

    Use methods to calculate the total purchase price

--- Program.cs
+++ Program.cs
@@ -4,7 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        // TODO: Create C# methods that return values
-        // https://learn.microsoft.com/en-us/training/modules/create-c-sharp-methods-return-values/
+        double total = 0;
+        double minimumSpend = 30.00;
+
+        double[] items = {15.97, 3.50, 12.25, 22.99, 10.98};
+        double[] discounts = {0.30, 0.00, 0.10, 0.20, 0.50};
+
+        Console.WriteLine($"Total: ${total}");
+
+        double GetDiscountedPrice(int itemIndex)
+        {
+            // Calculate the discounted price of the item
+            double result = items[itemIndex] * (1 - discounts[itemIndex]);
+            return result;
+        }
+
+        // Returning expression from Method
+        bool TotalMeetsMinimum()
+        {
+            // Check if the total meets the minimum
+            return total >= minimumSpend;
+        }
+
+        string FormatDecimal(double input)
+        {
+            // Format the double so only 2 decimal places are displayed
+            return input.ToString().Substring(0, 5);
+        }
+
     }
 }
