fc3bd0f2fe055c4b7df67f52ac392de2ef3943b9
mDestaFadilah <desta.08b@gmail.com>
2023-09-27 21:38:36 +0700

    Create a method that returns an integer

--- Program.cs
+++ Program.cs
@@ -4,45 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        double total = 0;
-        double minimumSpend = 30.00;
+        double usd = 23.73;
+        int idr = UsedToIdr(usd);
 
-        double[] items = {15.97, 3.50, 12.25, 22.99, 10.98};
-        double[] discounts = {0.30, 0.00, 0.10, 0.20, 0.50};
-        
-        for (int i = 0; i < items.Length; i++)
-        {
-            total += GetDiscountedPrice(i);
-        }
-    
-        // or ternary style: total -= TotalMeetsMinimum() ? 5.00 : 0.00;
-        if (TotalMeetsMinimum())
-        {
-            total -= 5.00;
-        }
-        Console.WriteLine($"Total: ${FormatDecimal(total)}");
+        Console.WriteLine($"${usd} USD = Rp.{idr} rupiah");
 
-        double GetDiscountedPrice(int itemIndex)
+        int UsedToIdr(double usd)
         {
-            // Calculate the discounted price of the item
-            double result = items[itemIndex] * (1 - discounts[itemIndex]);
-            return result;
+            int rate = 15000;
+            return (int) (rate * usd);
         }
-
-        // Returning expression from Method
-        bool TotalMeetsMinimum()
-        {
-            // Check if the total meets the minimum
-            return total >= minimumSpend;
-        }
-
-        string FormatDecimal(double input)
-        {
-            // Format the double so only 2 decimal places are displayed
-            return input.ToString().Substring(0, 5);
-        }
-
-        // Recap
 
     }
 }
