00ea6378a03898640d8d74c8aa197bb176407fcb
mDestaFadilah <desta.08b@gmail.com>
2023-09-27 21:33:10 +0700

    Capture the return values

--- Program.cs
+++ Program.cs
@@ -9,8 +9,18 @@ class Program
 
         double[] items = {15.97, 3.50, 12.25, 22.99, 10.98};
         double[] discounts = {0.30, 0.00, 0.10, 0.20, 0.50};
-
-        Console.WriteLine($"Total: ${total}");
+        
+        for (int i = 0; i < items.Length; i++)
+        {
+            total += GetDiscountedPrice(i);
+        }
+    
+        // or ternary style: total -= TotalMeetsMinimum() ? 5.00 : 0.00;
+        if (TotalMeetsMinimum())
+        {
+            total -= 5.00;
+        }
+        Console.WriteLine($"Total: ${FormatDecimal(total)}");
 
         double GetDiscountedPrice(int itemIndex)
         {
