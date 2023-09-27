bb6f597811295d46da042fe34ac38d796ecab5d5
mDestaFadilah <desta.08b@gmail.com>
2023-09-27 21:47:43 +0700

    PAUSE: https://learn.microsoft.com/en-us/training/modules/create-c-sharp-methods-return-values/4-exercise-create-methods-return-strings

--- Program.cs
+++ Program.cs
@@ -4,23 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        double usd = 23.73;
-        int idr = UsdToIdr(usd);
-
-        Console.WriteLine($"${usd} USD = Rp.{idr} rupiah");
-        Console.WriteLine($"Rp.{idr} IDR = ${IdrToUsd(idr)} USD");
-
-        int UsdToIdr(double usd)
-        {
-            int rate = 15500;
-            return (int) (rate * usd);
-        }
-
-        double IdrToUsd(int idr)
-        {
-            int rate = 15500;
-            return idr / rate;
-        }
-
+        // TODO: Create a method that returns a string
     }
 }
