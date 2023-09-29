0ba5bc401897d17ef924fbd1d9c48f875b878f17
mDestaFadilah <desta.08b@gmail.com>
2023-09-29 13:28:49 +0700

    Test your solution

--- Program.cs
+++ Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
+        int target = 60;
+        int[] coins = new int[] {5, 5, 50, 25, 25, 10, 5};
+        int[] result = TwoCoins(coins, target);
+
+        if (result.Length == 0)
+        {
+            Console.WriteLine("No two coins make change");
+        }
+        else
+        {
+            Console.WriteLine($"Change found at positions {result[0]} and {result[1]}");
+        }
+
         int[] TwoCoins(int[] coins, int target)
         {
             for (int curr = 0; curr < coins.Length; curr++)
