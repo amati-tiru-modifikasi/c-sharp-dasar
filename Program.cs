85d1322f869a4cc38e13f8a739644463ad4a6882
mDestaFadilah <desta.08b@gmail.com>
2023-09-30 14:40:59 +0700

    PAUSE: https://learn.microsoft.com/en-us/training/modules/guided-project-visit-petting-zoo/4-exercise-shuffle-array

--- Program.cs
+++ Program.cs
@@ -1,5 +1,17 @@
 ﻿using System;
 
+string[] pettingZoo = 
+{
+    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese", 
+    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws", 
+    "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
+};
+
+// RandomizeAnimals();
+// string[,] group = AssignGroup();
+Console.WriteLine("School A");
+// PrintGroup(group);
+
 class Program
 {
     static void Main(string[] args)
