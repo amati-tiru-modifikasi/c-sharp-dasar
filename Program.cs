e3613b0c10406cd9cd91e407d99e137e3e77755a
mDestaFadilah <desta.08b@gmail.com>
2023-10-01 22:04:57 +0700

    exercise-optional-parameters

--- Program.cs
+++ Program.cs
@@ -8,9 +8,27 @@ string[] pettingZoo =
 };
 
 RandomizeAnimals();
-// string[,] group = AssignGroup();
+string[,] group = AssignGroup();
 Console.WriteLine("School A");
 // PrintGroup(group);
+
+// we create 2D array dimension
+string[,] AssignGroup(int groups = 6)
+{
+    string[,] result = new string[groups, pettingZoo.Length/groups];
+
+    // start pettingZoo position
+    int start = 0;
+
+    for (int i = 0; i < groups; i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i,j] = pettingZoo[start++];
+        }
+    }
+    return result;
+}
 
 void RandomizeAnimals()
 {
