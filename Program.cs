41f1a32ca6ccaebaa9ee8aba955bf42bf34c37dc
mDestaFadilah <desta.08b@gmail.com>
2023-10-09 21:20:11 +0700

    RECAP You can find additional information about the Visual Studio Code debugger here: https://code.visualstudio.com/docs/editor/debugging.
    You can find additional information about the OmniSharp debugger for C# here: https://github.com/OmniSharp/omnisharp-vscode/blob/master/debugger.md.
    
    You can find additional information about the OmniSharp debugger and the launch.json file here: https://github.com/OmniSharp/omnisharp-vscode/blob/master/debugger-launchjson.md.
    
    You can find additional information about advanced launch configuration scenarios here: https://code.visualstudio.com/docs/editor/debugging.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿// TODO: Exercise - Monitor variables and execution flow
+﻿// RECAP
+
+// You can find additional information about the Visual Studio Code debugger here: https://code.visualstudio.com/docs/editor/debugging.
+
+// You can find additional information about the OmniSharp debugger for C# here: https://github.com/OmniSharp/omnisharp-vscode/blob/master/debugger.md.
+
+// You can find additional information about the OmniSharp debugger and the launch.json file here: https://github.com/OmniSharp/omnisharp-vscode/blob/master/debugger-launchjson.md.
+
+// You can find additional information about advanced launch configuration scenarios here: https://code.visualstudio.com/docs/editor/debugging.
