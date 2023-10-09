3f6660fd8da2428f352895a252a5b031394308b3
mDestaFadilah <desta.08b@gmail.com>
2023-10-09 21:21:20 +0700

    PAUSE: https://learn.microsoft.com/en-us/training/modules/implement-exception-handling-c-sharp/

--- Program.cs
+++ Program.cs
@@ -1,9 +1,1 @@
-﻿// RECAP
-
-// You can find additional information about the Visual Studio Code debugger here: https://code.visualstudio.com/docs/editor/debugging.
-
-// You can find additional information about the OmniSharp debugger for C# here: https://github.com/OmniSharp/omnisharp-vscode/blob/master/debugger.md.
-
-// You can find additional information about the OmniSharp debugger and the launch.json file here: https://github.com/OmniSharp/omnisharp-vscode/blob/master/debugger-launchjson.md.
-
-// You can find additional information about advanced launch configuration scenarios here: https://code.visualstudio.com/docs/editor/debugging.
+﻿// Implement Exception Handling in C# Console Applications
