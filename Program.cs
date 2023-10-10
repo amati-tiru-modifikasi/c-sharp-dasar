17843248746b365cc44cf6b20dec00f291776cb5
mDestaFadilah <desta.08b@gmail.com>
2023-10-10 13:04:30 +0700

    Exception handling process 1. Execution begins in the code block of the outer try statement. 2. An exception is thrown in the code block of the inner try statement. 3. The runtime finds the catch clause associated with the outer try statement. 4. Before the runtime transfers control to the first line of the catch code block, it executes the finally clause associated with the inner try statement. 5. The runtime then transfers control to the first line of the catch code block and executes the code that handles the exception.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
 ﻿try
 {
-    // try code block - code that may generate an exception
+    // Step 1: code execution begins
+    try
+    {
+        // Step 2: an exception occurs here
+    }
+    finally
+    {
+        // Step 4: the system executes the finally code block associated with the try statement where the exception occurred
+    }
+
 }
-catch
-{
-    // catch code block - code to handle an exception
-}
-finally
-{
-    // finally code block - code to clean up resources
+catch // Step 3: the system finds a catch clause that can handle the exception
+{   
+   // Step 5: the system transfers control to the first line of the catch code block
 }
