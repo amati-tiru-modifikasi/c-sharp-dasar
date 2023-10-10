66f994ea9da531f31074cd566235b355dbe5c430
mDestaFadilah <desta.08b@gmail.com>
2023-10-10 13:02:51 +0700

    Exception handling keywords, code blocks, and patterns

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿// Implement Exception Handling in C# Console Applications
+﻿try
+{
+    // try code block - code that may generate an exception
+}
+catch
+{
+    // catch code block - code to handle an exception
+}
+finally
+{
+    // finally code block - code to clean up resources
+}
