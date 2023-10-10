e17047b3274f5d40798c5d939a485ca442459ddd
mDestaFadilah <desta.08b@gmail.com>
2023-10-10 13:18:16 +0700

    Code samples for compiler-generated exceptions

--- Program.cs
+++ Program.cs
@@ -1,6 +1,43 @@
-﻿// // RECAP
+﻿// Code samples for compiler-generated exceptions
 
-//  - Common scenarios that may require exception handling include user input, data processing, file I/O operations, database operations, and network communication.
-//  - Exception handling in C# is implemented using try, catch, and finally keywords. Each keyword has an associated code block that serves a specific purpose.
-//  - Exceptions are represented as types and derived from the System.Exception class in .NET. Exceptions contain information that identifies the type of exception, and properties that provide additional details.
-//  - When an exception occurs, the .NET runtime searches for the nearest catch clause that can handle it. The search starts with the method where the exception was thrown, and moves down the call stack if necessary.
+// ArrayTypeMismatchException
+string[] names = { "Haikal", "Ziyad", "Arshaka" };
+object[] objj = (object[])names;
+
+object obj = (object)13;
+objs[2] = obj; 
+
+
+// DivideByZeroException
+int number1 = 3000;
+int number2 = 0;
+Console.WriteLine(number1 / number2);
+
+
+// FormatException
+int valueEntered;
+string userValue = "two";
+valueEntered = int.Parse(userValue);
+
+
+// IndexOutOfRangeException
+int[] values1 = { 3, 6, 9, 12, 15, 18, 21 };
+int[] values2 = new int[6];
+values2[values1.Length - 1] = values1[values1.Length - 1];
+
+
+// NullReferenceException
+int[] values = null;
+for (int i=0; i<=9; i++)
+    values[i] = i * 2;
+
+string? lowCaseString = null;
+Console.WriteLine(lowCaseString.ToUpper());
+
+
+// OverflowException
+decimal x = 400;
+byte i;
+
+i = (byte) x;
+Console.WriteLine(i);
