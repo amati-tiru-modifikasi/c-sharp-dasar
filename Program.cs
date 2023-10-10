3c7cb39fbe783d34bfbf5978d37cc3a47d6d2e54
mDestaFadilah <desta.08b@gmail.com>
2023-10-11 02:24:34 +0700

    TRY CATCH EXAMPLE

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
 ﻿// TODO: Exercise - Implement try-catch exception handling
+
+double float1 = 3000.0;
+double float2 = 0.0;
+int number1 = 3000;
+int number2 = 0;
+
+try
+{
+    Console.WriteLine(float1 / float2);
+    Console.WriteLine(number1 / number2);  
+}
+catch (System.Exception)
+{
+    Console.WriteLine("Exit program");
+}
