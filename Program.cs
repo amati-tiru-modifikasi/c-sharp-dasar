f6d86d1f970b2306e3ebb6fecfc902916c311ad1
mDestaFadilah <desta.08b@gmail.com>
2023-10-10 13:06:06 +0700

    RECAP  - Common scenarios that may require exception handling include user input, data processing, file I/O operations, database operations, and network communication.  - Exception handling in C# is implemented using try, catch, and finally keywords. Each keyword has an associated code block that serves a specific purpose.  - Exceptions are represented as types and derived from the System.Exception class in .NET. Exceptions contain information that identifies the type of exception, and properties that provide additional details.  - When an exception occurs, the .NET runtime searches for the nearest catch clause that can handle it. The search starts with the method where the exception was thrown, and moves down the call stack if necessary.

--- Program.cs
+++ Program.cs
@@ -1,17 +1,6 @@
-﻿try
-{
-    // Step 1: code execution begins
-    try
-    {
-        // Step 2: an exception occurs here
-    }
-    finally
-    {
-        // Step 4: the system executes the finally code block associated with the try statement where the exception occurred
-    }
+﻿// // RECAP
 
-}
-catch // Step 3: the system finds a catch clause that can handle the exception
-{   
-   // Step 5: the system transfers control to the first line of the catch code block
-}
+//  - Common scenarios that may require exception handling include user input, data processing, file I/O operations, database operations, and network communication.
+//  - Exception handling in C# is implemented using try, catch, and finally keywords. Each keyword has an associated code block that serves a specific purpose.
+//  - Exceptions are represented as types and derived from the System.Exception class in .NET. Exceptions contain information that identifies the type of exception, and properties that provide additional details.
+//  - When an exception occurs, the .NET runtime searches for the nearest catch clause that can handle it. The search starts with the method where the exception was thrown, and moves down the call stack if necessary.
