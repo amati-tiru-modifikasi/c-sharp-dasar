c392b87548bde4bc7bf8b1d9fd98c1f6af51d6b8
mDestaFadilah <desta.08b@gmail.com>
2023-10-16 11:11:44 +0700

    Catch a specific exception type

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ static void Process1()
     {
         WriteMessage();
     }
-    catch (Exception ex)
+    catch (DivideByZeroException ex)
     {
         Console.WriteLine($"Exception caught in Process1: {ex.Message}");
     }
@@ -28,7 +28,14 @@ static void WriteMessage()
     double float2 = 0.0;
     int number1 = 3000;
     int number2 = 0;
+    byte smallNumber;
 
     Console.WriteLine(float1 / float2);
-    Console.WriteLine(number1 / number2);
+    // Console.WriteLine(number1 / number2);
+    // Integral type calculations that are not inside a checked code block are treated as if they are inside an unchecked code block.
+    checked
+    {
+        smallNumber = (byte)number1;
+    }
+
 }
