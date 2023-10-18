f41c3575d7b3578dcf5285a287055637b0db1b4d
mDestaFadilah <desta.08b@gmail.com>
2023-10-18 11:24:27 +0700

    Exercise - Complete a challenge activity for creating and throwing exceptions

--- Program.cs
+++ Program.cs
@@ -1,36 +1,91 @@
-﻿// Prompt user
-Console.Write("Enter the lower bound: ");
-int lowerBound = int.Parse(Console.ReadLine());
-
-Console.Write("Enter the upper bound: ");
-int upperBound = int.Parse(Console.ReadLine());
-
-decimal averageValue = 0;
-
-// calculate the sum of the even numbers between the bound
-averageValue = AverageOfEvenNumbers(lowerBound, upperBound);
-
-// display value returned by averate of even number in the console
-Console.WriteLine($"The average of even number between {lowerBound} and {upperBound} is {averageValue}");
-
-// Wait for user input 
-Console.ReadLine();
-
-static decimal AverageOfEvenNumbers(int lowerBound, int upperBound)
+﻿string[][] userEnteredValues = new string[][]
 {
-    int sum = 0;
-    int count = 0;
-    decimal average = 0;
+            new string[] { "1", "2", "3"},
+            new string[] { "1", "two", "3"},
+            new string[] { "0", "1", "2"}
+};
 
-    for (int i = lowerBound; i <= upperBound; i++)
+string overallStatusMessage = "";
+
+overallStatusMessage = Workflow1(userEnteredValues);
+
+if (overallStatusMessage == "operating procedure complete")
+{
+    Console.WriteLine("'Workflow1' completed successfully.");
+}
+else
+{
+    Console.WriteLine("An error occurred during 'Workflow1'.");
+    Console.WriteLine(overallStatusMessage);
+}
+
+static string Workflow1(string[][] userEnteredValues)
+{
+    string operationStatusMessage = "good";
+    string processStatusMessage = "";
+
+    foreach (string[] userEntries in userEnteredValues)
     {
-        if (i % 2 == 0)
+        processStatusMessage = Process1(userEntries);
+
+        if (processStatusMessage == "process complete")
         {
-            sum += i;
-            count++;
+            Console.WriteLine("'Process1' completed successfully.");
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine("'Process1' encountered an issue, process aborted.");
+            Console.WriteLine(processStatusMessage);
+            Console.WriteLine();
+            operationStatusMessage = processStatusMessage;
         }
     }
-    average = (decimal)sum / count;
-    return average;
 
+    if (operationStatusMessage == "good")
+    {
+        operationStatusMessage = "operating procedure complete";
+    }
+
+    return operationStatusMessage;
+}
+
+static string Process1(String[] userEntries)
+{
+    string processStatus = "clean";
+    string returnMessage = "";
+    int valueEntered;
+
+    foreach (string userValue in userEntries)
+    {
+        bool integerFormat = int.TryParse(userValue, out valueEntered);
+
+        if (integerFormat == true)
+        {
+            if (valueEntered != 0)
+            {
+                checked
+                {
+                    int calculatedValue = 4 / valueEntered;
+                }
+            }
+            else
+            {
+                returnMessage = "Invalid data. User input values must be non-zero values.";
+                processStatus = "error";
+            }
+        }
+        else
+        {
+            returnMessage = "Invalid data. User input values must be valid integers.";
+            processStatus = "error";
+        }
+    }
+
+    if (processStatus == "clean")
+    {
+        returnMessage = "process complete";
+    }
+
+    return returnMessage;
 }
